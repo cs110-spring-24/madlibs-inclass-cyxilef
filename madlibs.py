1e3cf9a56968586d6d6b983e652ae391362524f4
cyxilef <157657991+cyxilef@users.noreply.github.com>
2024-02-23 18:47:25 -0800

    Completed the work on time and thought I commited

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,19 @@
 # TODO Create your inputs, variables, and add your story here!
+# Madlibs
+
+noun = input("Enter a Noun: ")
+adjective = input("Enter an Adjective: ")
+verb = input("Enter a Verb: ")
+
+print(f"{noun} was {adjective} while {verb}")
+
+noun2 = input("Enter a Noun: ")
+adjective2 = input("Enter an Adjective: ")
+verb2 = input("Enter a Verb: ")
+print(f"{noun2} threw {adjective2} while {verb2}")
+
+noun3 = input("Enter a Noun: ")
+adjective3 = input("Enter an Adjective: ")
+verb3 = input("Enter a Verb: ")
+noun4 = input("Enter a Noun: ")
+print(f"{noun3} ate {adjective3} while {verb3} and {noun4}")
